2e9ab456f3f223cf63777235a02afb2c01a46482
Kicka5h <me@kicka5h.io>
2020-02-06 20:27:07 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,9 +13,13 @@ namespace Ex_4._2_Coins
             CanRack sodaRack = new CanRack();
             DisplayRack showFlavors = new DisplayRack();
 
+            Console.WriteLine();
             Console.WriteLine("=================================");
             Console.WriteLine("Welcome to the C# Vending Machine");
             Console.WriteLine("=================================");
+            Console.WriteLine("===Today's Soda Price is $.025===");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
 
             bool exitVend = false;
             do
@@ -31,20 +35,25 @@ namespace Ex_4._2_Coins
 
                     if (checkValue == false)
                     {
-                        Console.Write("Please insert a coin: ");
+                        Console.Write("Please insert coins one at a time: ");
+                        Console.WriteLine();
 
                         string coinNameInserted = Console.ReadLine().ToUpper();
                         Coin coinInserted = new Coin(coinNameInserted);
 
                         Console.WriteLine("You have inserted a {0} worth {1:c}", coinInserted, coinInserted.ValueOf);
+                        Console.WriteLine();
 
                         totalValueInserted += coinInserted.ValueOf;
                         Console.WriteLine("Total value inserted is {0:c}", totalValueInserted);
+                        Console.WriteLine();
                     }
                 }
-                
-                Console.WriteLine("You have inserted {0:c}", totalValueInserted);
+
+                Console.WriteLine("Thanks, you have now inserted {0:c}", totalValueInserted);
+                Console.WriteLine();
                 Console.WriteLine($"Please choose your flavor: {showFlavors.DisplayCanRackString()}");
+                Console.WriteLine();
 
                 string choseFlavor = Console.ReadLine();
                 string chosenFlavor = choseFlavor.ToUpper();
@@ -63,18 +72,23 @@ namespace Ex_4._2_Coins
                         {
                             sodaRack.RemoveACanOf(chosenFlavor);
                             Console.WriteLine($"Thanks, here is your {chosenFlavor} soda.");
-                        }
-                        else
+                            Console.WriteLine();
+                    }
+                    else
                         {
                             Console.WriteLine($"Sorry, there is no more {chosenFlavor} flavor soda.");
-                        }
+                            Console.WriteLine();
+                    }
                 }
                 else
                 {
                     Console.WriteLine($"Sorry, {chosenFlavor} is not an available flavor.");
+                    Console.WriteLine();
                 }
 
                 Console.WriteLine("Would you like to exit the vending machine? (y/n)");
+                Console.WriteLine();
+
                 string response = Console.ReadLine();
                 
                 if (response == "y")
